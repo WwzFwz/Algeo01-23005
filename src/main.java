b5b8d58c413f7da7fa62735a2c2ad22e9de1dbb2
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-10-10 11:22:51 +0700

    fix bug: nama variabel disamakan

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import matrix.Matrix ;
+public class  Main {
+    public static void main (String[] args){
+        Matrix m = Matrix.readMatrixFromKeyboard();
+        m.displayMatrix();
+    }
+}
+
