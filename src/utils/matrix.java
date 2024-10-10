cdea6570d6a17af658a1e0483a7c42d42d296796
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-10-10 07:20:12 +0700

    feat copy,display,input keyboard

--- src/utils/matrix.java
+++ src/utils/matrix.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class Matrix {
-    private final int col;
-    private final int row;
-    private final double [][] data;
+    private int col;
+    private int row;
+    private double [][] data;
 
     // CONSTRUCTOR
 
@@ -60,13 +61,70 @@ public class Matrix {
 
 
     // IO
+
     //readMatrixfromFile
 
     //readMatrixfromKeyboard
+    public static Matrix readMatrixFromKeyboard() {
+        int i;j
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris: ");
+        int row = input.nextInt();
+        System.out.print("Masukkan jumlah kolom: ");
+        int col = input.nextInt();
+
+        Matrix matrix = new Matrix(row, col);
+        System.out.println("Masukkan elemen matriks:");
+
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                System.out.print("Elemen [" + i + "][" + j + "]: ");
+                matrix.setElement(i, j, input.nextDouble());
+            }
+        }
+        return matrix;
+    }
+    // Menampilkan matriks
+    public void displayMatrix() {
+        for (int i = 0; i < this.row; i++) {
+            for (int j = 0; j < this.col; j++) {
+                System.out.print(this.data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 
-    //displayMatrix
 
 
-}
+    // VALIDATION
+
+
+    public boolean isSquareMatrix() {
+        return this.row == this.col;
+    }
+
+
+
+
+    // OTHERS UTILITIES
+
+    // Membuat salinan matrix 
+    public Matrix copyMatrix() {
+        Matrix copy = new Matrix(this.row, this.col);
+
+        for (int i = 0; i < this.row; i++) {
+            System.arraycopy(this.data[i],0,copy.data[i],0,this.col);
+        }
+
+        return copy;
+    }
+
+    }
+
+
+  
+
+
+
 
