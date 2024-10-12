d1453f05ec1cb7f69673b6b915b542de3685e75f
malfansya <163796625+malfansya@users.noreply.github.com>
2024-10-12 12:07:08 +0700

    feat Gauss Jordan

---
+++ src/matrix/GaussJordan.java
@@ -0,0 +1,29 @@
+package matrix
+
+public class GaussJordan{
+    // asumsi inputan udah berupa augmented matrix
+    public static Matrix GaussJordan(Matrix matrix){
+        int row = matrix.getRowLength();  
+        int col = matrix.getColLength(); 
+
+        // bikin diagonal nya 1
+        for (int i=0 ; i<row ; i ++){
+            double diagonal = matrix.getElmt(i, i);
+            // bikin diagonal nya 1
+            for (int j = 0; j<row ; j++){
+                matrix.setElmt(i,j,matrix.getElmt(i, j)/diagonal);
+
+                for (int k = 0; k<col ; k++){
+                    if(i!=j){
+                        double pengali = matrix.getElmt(j, i);
+                        matrix.setElmt(j, k, (matrix.getElmt(j,k) - matrix.getElmt(i,k) * pengali));
+                    }
+                }
+            }
+            
+        }
+
+        return matrix.getColElmts(col);
+
+    }
+}
