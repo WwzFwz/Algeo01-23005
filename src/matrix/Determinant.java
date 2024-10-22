832475b70f3571ea1125608ca63438a8f1a3e558
RNXFreeze <m.raihannazhimoktana@gmail.com>
2024-10-22 18:01:02 +0700

    feat: add determinant

---
+++ src/matrix/Determinant.java
@@ -0,0 +1,171 @@
+// PROGRAM K14-GeprekMumbul-F04
+
+// IDENTITAS
+// Kelompok : 14 - Geprek Mumbul
+// NIM/Nama - 1 : 13523021 - Muhammad Raihan Nazhim Oktana
+// NIM/Nama - 2 : 13523005 - Muhammad Alfansya
+// NIM/Nama - 3 : 13523065 - Dzaky Aurelia Fawwaz
+// Instansi : Sekolah Teknik Elektro dan Informatika (STEI) Institut Teknologi Bandung (ITB)
+// Jurusan : Teknik Informatika (IF)
+// Nama File : Determinant.java
+// Topik : Tugas Besar 1 Aljabar Linier dan Geometri 2024 (IF2123-24)
+// Tanggal : Senin, 21 Oktober 2024
+// Deskripsi : Subprogram F04 - Determinant
+// Penanggung Jawab F04 : 13523021 - Muhammad Raihan Nazhim Oktana
+
+// KAMUS
+// matrix : package
+// Determinant : class
+// DeterminantCOFACTOR , DeterminantOBE : function
+
+// ALGORITMA
+package matrix;
+public class Determinant {
+    public static double DeterminantCOFACTOR (Matrix matrix) {
+        // SPESIFIKASI LOKAL
+        // Menghitung determinan dengan kofaktor.
+
+        // KAMUS LOKAL
+        // getRow , getCol , getElmt , setElmt : procedure
+        // matrix , part : Matrix
+        // i , j , k , col : integer
+        // ans : double
+
+        // ALGORITMA LOKAL
+        double ans = 0;
+        if ((matrix.getRow() <= 0) || (!matrix.isSquareMatrix())) {
+            throw new IllegalArgumentException("Matrix tidak valid atau tidak berbentuk persegi.");
+        }
+        if (matrix.getRow() == 1) {
+            return matrix.getElmt(0 , 0);
+        }
+        for (int i = 0 ; i < matrix.getCol() ; i++) {
+            Matrix part = new Matrix(matrix.getRow() - 1 , matrix.getCol() - 1);
+            for (int j = 1 ; j < matrix.getRow() ; j++) {
+                int col = 0;
+                for (int k = 0 ; k < matrix.getCol() ; k++) {
+                    if (k != i) {
+                        part.setElmt(j - 1 , col , matrix.getElmt(j , k));
+                        col++;
+                    }
+    
+                }
+            }
+            if (i % 2 == 0) {
+                ans += matrix.getElmt(0 , i) * DeterminantCOFACTOR(part);
+            } else {
+                ans -= matrix.getElmt(0 , i) * DeterminantCOFACTOR(part);
+            }
+        }
+        return ans;
+    }
+
+    public static double DeterminantCOFACTORPass (Matrix matrix , int row , int col) {
+        // SPESIFIKASI LOKAL
+        // Menghitung determinan suatu matrix bagian yang mengabaikan suatu row dan col.
+
+        // KAMUS LOKAL
+        // getRow , getCol , getElmt , setElmt : procedure
+        // i , j , n , row , col : integer
+        // matrix , ans : Matrix
+
+        // ALGORITMA LOKAL
+		int n = matrix.getRow();
+        Matrix ans = new Matrix(n - 1 , n - 1);
+        int i = 0;
+        while (i < n) {
+            if (i == row) {
+                i++;
+                continue;
+            }
+            int j = 0;
+            while (j < n) {
+                if (j == col) {
+                    j++;
+                    continue;
+                }
+                if (i < row) {
+                    if (j < col) {
+						ans.setElmt(i , j , matrix.getElmt(i , j));
+                    }
+					else {
+						ans.setElmt(i , j - 1 , matrix.getElmt(i , j));
+                    }
+				}
+				else{
+					if (j < col) {
+						ans.setElmt(i - 1 , j , matrix.getElmt(i , j));
+                    }
+					else {
+						ans.setElmt(i - 1 , j - 1 , matrix.getElmt(i , j));
+                    }
+				}
+                j++;
+            }
+            i++;
+        }
+		return DeterminantCOFACTOR(ans);
+    }
+
+    public static double DeterminantOBE (Matrix matrix) {
+        // SPESIFIKASI LOKAL
+        // Menghitung determinan dengan OBE (Operasi Baris Elementer).
+
+        // KAMUS LOKAL
+        // getRow , getCol , getElmt , setElmt , isSquareMatrix : procedure
+        // matrix : Matrix
+        // i , j , k , l , row , col : integer
+        // x , y , temp , ans : double
+
+        // ALGORITMA LOKAL
+        int i , j , k , l;
+        double ans = 1;
+        double x , y;
+        int row = matrix.getRow();
+        int col = matrix.getCol();
+        if (matrix.isSquareMatrix()) {
+            if (row == 1 || col == 1) {
+                return matrix.getElmt(0 , 0);
+            } else {
+                for (i = 0 ; i < row ; i++) {
+                    if (i < row) {
+                        if (matrix.getElmt(i , i) == 0) {
+                            j = i + 1;
+                            while (j < row && matrix.getElmt(j , i) == 0) {
+                                j++;
+                            }
+                            if (j < row) {
+                                ans *= -1;
+                                double temp;
+                                for (k = 0 ; k < row ; k++) {
+                                    temp = matrix.getElmt(i , k);
+                                    matrix.setElmt(i , k , matrix.getElmt(j , k));
+                                    matrix.setElmt(j , k , temp);
+                                }
+                            } else {
+                                ans = 0;
+                                break;
+                            }
+                        }
+                        x = matrix.getElmt(i , i);
+                        ans *= x;
+                        if (x != 0) {
+                            for (j = 0 ; j < row ; j++) {
+                                matrix.setElmt(i , j , matrix.getElmt(i , j) / x);
+                            }
+                        }
+                        for (k = i + 1 ; k < row ; k++) {
+                            y = matrix.getElmt(k , i);
+                            for (l = 0 ; l < row ; l++) {
+                                matrix.setElmt(k , l , matrix.getElmt(k , l) - (y * matrix.getElmt(i , l)));
+                            }
+                        }
+                    }
+                }
+                return ans;
+            }
+        } else {
+            return Double.NaN;
+        }
+    }
+}
